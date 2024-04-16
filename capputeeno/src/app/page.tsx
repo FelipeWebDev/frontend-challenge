ab709ee5af83c25dab3f23cbdfc5779f75ad75a2
Felipe Rodrigues <fehliveon@live.com>
2024-04-16 14:51:50 -0300

    feat: initial product fetch

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -1,8 +1,27 @@
+"use client";
+
 import Image from "next/image";
-import styles from "./page.module.css";
+import FilterBar from "@/components/FilterMenu/FilterBar";
+import styled from "styled-components";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import ProductList from "@/components/Common/ProductList";
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding: 34px 160px;
+  min-height: 100vh;
+`;
 
 export default function Home() {
+  const client = new QueryClient()
+
   return (
-    <></>
+    <QueryClientProvider client={client}>
+      <Main>
+        <FilterBar />
+        <ProductList />
+      </Main>
+    </QueryClientProvider>
   );
 }
