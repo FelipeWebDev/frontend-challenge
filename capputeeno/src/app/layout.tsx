a3917c7b6de21d15a0f041f936d6e7ffbf354dfb
Felipe Rodrigues <fehliveon@live.com>
2024-04-17 15:19:41 -0300

    fix: small fix

--- capputeeno/src/app/layout.tsx
+++ capputeeno/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Saira } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
-import { FilterContextProvider } from "@/contexts/FilterContext";
 import DefaultProviders from "@/components/Commons/DefaultProviders";
 
 const saira = Saira({
