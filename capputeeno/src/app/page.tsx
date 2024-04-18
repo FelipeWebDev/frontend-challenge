21a05d77c04041ba088cbcc18e80b8d68161d878
Felipe Rodrigues <fehliveon@live.com>
2024-04-18 16:00:27 -0300

    feat: default layout configuration

--- capputeeno/src/app/page.tsx
+++ capputeeno/src/app/page.tsx
@@ -3,24 +3,21 @@
 import FilterBar from "@/components/FilterMenu/FilterBar";
 import styled from "styled-components";
 import ProductList from "@/components/ProductList/ProductList";
+import { DefaultPageLayout } from "@/components/Commons/DefaultPageLayout";
 
 const PageWrapper = styled.main`
   display: flex;
   flex-direction: column;
-  min-height: 100vh;
-  background-color: var(--bg-primary);
-  padding: 12px 24px;
-
-  @media (min-width: ${props => props.theme.desktopBreakpoint}) {
-    padding: 34px 160px;
-  }
+  align-items: center;
 `;
 
 export default function Home() {
   return (
+    <DefaultPageLayout>
       <PageWrapper>
         <FilterBar />
         <ProductList />
       </PageWrapper>
+    </DefaultPageLayout>
   );
 }
